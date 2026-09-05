90b482ddd8981cc8f4964d11a448e23f0ddb041c
immgk18 <immgk11@gmail.com>
2026-09-05 12:52:59 +0530

    Update IC.py

--- IC.py
+++ IC.py
@@ -52,6 +52,12 @@ os.makedirs(
 # ================================================================
 
 app = Flask(__name__)
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    return response
 
 app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024
 
